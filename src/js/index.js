d09e428b61c5ff50c6cc90df49fdde52909acb10
VHolodiuk <MALNWTI1995@gmail.com>
2020-10-27 16:37:49 +0200

    Full update

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded', function() {
+
+})
+window.addEventListener('load', function() {
+    console.log('js work');
+})
+window.addEventListener('resize', function() {
+    
+})
